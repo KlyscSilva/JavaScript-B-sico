2f025db887b406c0211d4010cfe182e1a89483ed
KlyscSilva <silva12354@gmail.com>
2022-12-11 22:08:29 -0300

    operadores aritméticos

---
+++ operadores-ari.js
@@ -0,0 +1,39 @@
+/*Operadores Aritméticos
+
++ = adição
+- = subtração
+* = multiplicação
+/ = divisão
+% = resto
+++ = Incremento
+-- = Decremento
+** = exponecial
+
+Exemplos abaixo de cada operador aritmético
+
+*/
+
+
+let adi = 10 + 20
+console.log(adi);
+
+let sub = 20 - 50
+console.log(sub);
+
+let multi = 50 * 2
+console.log(multi);
+
+let div = 100 / 2
+console.log(div);
+
+let res = 100%2
+console.log(res)
+
+let inc = 10
+console.log(++inc);
+
+let dec = 20
+console.log(--dec);
+
+let exp = 20 ** 2
+console.log(exp);
